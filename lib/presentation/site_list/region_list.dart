09d85509032e96c22cf8aa64a7f718955365c75c
Dhiraj wable <dhiraj@eyvo.com>
2024-12-04 12:11:10 +0530

    changes

--- lib/presentation/site_list/region_list.dart
+++ lib/presentation/site_list/region_list.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:eyvo_inventory/api/api_service/api_service.dart';
+import 'package:eyvo_inventory/api/api_service/bloc.dart';
 import 'package:eyvo_inventory/api/response_models/region_response.dart';
 import 'package:eyvo_inventory/app/app_prefs.dart';
 import 'package:eyvo_inventory/app/sizes_helper.dart';
@@ -42,21 +43,32 @@ class _RegionListViewState extends State<RegionListView> {
       isLoading = true;
     });
 
-    Map<String, dynamic> data = {
-      'uid': SharedPrefs().uID,
-    };
-    final jsonResponse =
-        await apiService.postRequest(context, ApiService.regionList, data);
-    if (jsonResponse != null) {
-      final response = RegionResponse.fromJson(jsonResponse);
-      if (response.code == '200') {
-        setState(() {
-          regionItems = response.data;
-        });
-      } else {
-        isError = true;
-        errorText = response.message.join(', ');
-      }
+    // Map<String, dynamic> data = {
+    //   'uid': SharedPrefs().uID,
+    // };
+    //final jsonResponse =
+    //     await apiService.postRequest(context, ApiService.regionList, data);
+    // if (jsonResponse != null) {
+    //   final response = RegionResponse.fromJson(jsonResponse);
+    //   if (response.code == '200') {
+    //     setState(() {
+    //       regionItems = response.data;
+    //     });
+    //   } else {
+    //     isError = true;
+    //     errorText = response.message.join(', ');
+    //   }
+    // }
+
+    var res = await globalBloc.doFetchRegionOfUser(context, SharedPrefs().uID);
+
+    if (res.code == '200') {
+      setState(() {
+        regionItems = res.data;
+      });
+    } else {
+      isError = true;
+      errorText = res.message.join(', ');
     }
 
     setState(() {
@@ -139,50 +151,50 @@ class _RegionListViewState extends State<RegionListView> {
                             padding: const EdgeInsets.only(
                                 left: 18, right: 18, bottom: 18),
                             child: Container(
-                                decoration: BoxDecoration(
-                                    color: ColorManager.white,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: ListView.separated(
-                                  itemCount: regionItems.length,
-                                  separatorBuilder: (context, index) => Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 0.0),
-                                    child: Container(
-                                      height: 1.5,
-                                      decoration: BoxDecoration(
-                                        color: ColorManager.primary,
-                                      ),
+                              decoration: BoxDecoration(
+                                  color: ColorManager.white,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: ListView.separated(
+                                itemCount: regionItems.length,
+                                separatorBuilder: (context, index) => Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 0.0),
+                                  child: Container(
+                                    height: 1.5,
+                                    decoration: BoxDecoration(
+                                      color: ColorManager.primary,
                                     ),
                                   ),
-                                  itemBuilder: (context, index) {
-                                    final item = regionItems[index];
-                                    return Padding(
-                                      padding: const EdgeInsets.all(0.0),
-                                      child: ListTile(
-                                        title: Text(item.regionCode,
-                                            style: item.regionCode ==
-                                                    widget.selectedItem
-                                                ? getMediumStyle(
-                                                    color: ColorManager.orange2,
-                                                    fontSize: FontSize.s23_25)
-                                                : getRegularStyle(
-                                                    color:
-                                                        ColorManager.lightGrey2,
-                                                    fontSize: FontSize.s23_25)),
-                                        onTap: () {
-                                          SharedPrefs().selectedRegion =
-                                              item.regionCode;
-                                          SharedPrefs().selectedRegionID =
-                                              item.regionId;
-                                          showSnackBar(context,
-                                              '${AppStrings.siteSelectedMessage}${item.regionCode}');
-                                          Navigator.pop(
-                                              context, item.regionCode);
-                                        },
-                                      ),
-                                    );
-                                  },
-                                )),
+                                ),
+                                itemBuilder: (context, index) {
+                                  final item = regionItems[index];
+                                  return Padding(
+                                    padding: const EdgeInsets.all(0.0),
+                                    child: ListTile(
+                                      title: Text(item.regionCode,
+                                          style: item.regionCode ==
+                                                  widget.selectedItem
+                                              ? getMediumStyle(
+                                                  color: ColorManager.orange2,
+                                                  fontSize: FontSize.s23_25)
+                                              : getRegularStyle(
+                                                  color:
+                                                      ColorManager.lightGrey2,
+                                                  fontSize: FontSize.s23_25)),
+                                      onTap: () {
+                                        SharedPrefs().selectedRegion =
+                                            item.regionCode;
+                                        SharedPrefs().selectedRegionID =
+                                            item.regionId;
+                                        showSnackBar(context,
+                                            '${AppStrings.siteSelectedMessage}${item.regionCode}');
+                                        Navigator.pop(context, item.regionCode);
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
                           ),
                         )
                       ],
